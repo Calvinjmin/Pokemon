2bf7103421818a715e5c04714f997d1e529bc2c1
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2021-10-16 21:47:08 -0400

    Pokemon Database: Snorlax Addition

--- pokemonDatabase.py
+++ pokemonDatabase.py
@@ -7,8 +7,15 @@ Dragonite_Moveset = [Dragon_Claw, Dragon_Pulse, Fly, Hyper_Beam]
 Gengar_Moveset = [Shadow_Ball, Thunderbolt, Giga_Drain, Sludge_Bomb]
 Lapras_Moveset = [Thunderbolt, Hydro_Pump, Surf, Ice_Beam]
 
-# Creating Pokemon
+# Normal Type
+Snorlax = Pokemon("Snorlax", "Normal", [], 750, 50)
+
+# Fire Type
 Charizard = Pokemon("Charizard", "Fire", Charizard_Moveset, 500, 50)
+
+# Dragon Type
 Dragonite = Pokemon("Dragonite", "Dragon", Dragonite_Moveset, 400, 50)
+
+# Dual-Typing
 Gengar = Pokemon("Gengar", "Ghost/Poison", Gengar_Moveset, 500, 50)
 Lapras = Pokemon("Lapras", "Ice/Water", Lapras_Moveset, 500, 50)
